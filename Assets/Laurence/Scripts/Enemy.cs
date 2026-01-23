1f8a432177cf4686c93c876eab17de42740b8cee
LaurenceAllenAbertay <2301131@uad.ac.uk>
2026-01-23 17:33:44 +0000

    Added little enemy guy.
    Doesn't do much but has his own outline.

---
+++ Assets/Laurence/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Stats")]
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public delegate void HealthChanged(int current, int max);
+    public event HealthChanged OnHealthChanged;
+
+    public delegate void EnemyDied(Enemy enemy);
+    public event EnemyDied OnDied;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        OnHealthChanged?.Invoke(currentHealth, maxHealth);
+
+        Debug.Log($"{gameObject.name} took {damage} damage. HP: {currentHealth}/{maxHealth}");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        currentHealth = Mathf.Min(currentHealth + amount, maxHealth);
+        OnHealthChanged?.Invoke(currentHealth, maxHealth);
+    }
+
+    private void Die()
+    {
+        Debug.Log($"{gameObject.name} died!");
+        OnDied?.Invoke(this);
+        Destroy(gameObject);
+    }
+}
