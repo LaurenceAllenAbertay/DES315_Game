9c43c58b2d4b18fb0e46c2d17b81822a3d4f03ff
AverageMarbleEnjoyer <2203180@uad.ac.uk>
2026-04-16 14:36:53 +0100

    Wwise added, blood storm has sound now
    After a long stretch of problems, the most recent build has Wwise working. The first was to add a working blood rain sound.

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,89 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BLOODSTORM = 2566684358U;
+        static const AkUniqueID BRUTEATTACK = 1580047315U;
+        static const AkUniqueID BRUTEBLOCK = 1416938370U;
+        static const AkUniqueID BRUTEHEAL = 1506802029U;
+        static const AkUniqueID BRUTEHURT = 1185467356U;
+        static const AkUniqueID BRUTEMOVE = 2186417968U;
+        static const AkUniqueID CARDUI_CONFIRM = 4049969704U;
+        static const AkUniqueID CARDUI_END = 982717551U;
+        static const AkUniqueID CARDUI_PICK = 2669983563U;
+        static const AkUniqueID CARDUI_SHUFFLE = 2654324891U;
+        static const AkUniqueID CARDUI_START = 2185107916U;
+        static const AkUniqueID CHESTCLOSE = 1044338672U;
+        static const AkUniqueID CHESTOPEN = 2892697348U;
+        static const AkUniqueID COINMISS = 1138653966U;
+        static const AkUniqueID COINSPEND = 1406895930U;
+        static const AkUniqueID COINTOSS = 767552063U;
+        static const AkUniqueID DOOROPEN = 1404805401U;
+        static const AkUniqueID FIREFLINTLOCK = 1820443583U;
+        static const AkUniqueID FOOTSTEPS = 2385628198U;
+        static const AkUniqueID GAMEOVER = 4158285989U;
+        static const AkUniqueID PLAYERDEATH = 1656947812U;
+        static const AkUniqueID PLAYERHEAL = 3216143520U;
+        static const AkUniqueID PLAYERHURT = 3537581393U;
+        static const AkUniqueID PLAYERLAUGH = 2348858765U;
+        static const AkUniqueID PLAYERMELEEATTACK = 1598433574U;
+        static const AkUniqueID TORCHBREAK = 717429066U;
+        static const AkUniqueID TORCHLOOP = 4171534483U;
+        static const AkUniqueID UI_CLOSE = 2519197294U;
+        static const AkUniqueID UI_NEXTPAGE = 1855554456U;
+        static const AkUniqueID UI_OPEN = 3282376362U;
+        static const AkUniqueID UI_PREVPAGE = 896450232U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace FOOTSTEP_TYPE
+        {
+            static const AkUniqueID GROUP = 2615620554U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GRASSHEAVY = 2525268562U;
+                static const AkUniqueID GRASSLIGHT = 2677407975U;
+                static const AkUniqueID GRAVELHEAVY = 2097603749U;
+                static const AkUniqueID GRAVELLIGHT = 2548727596U;
+                static const AkUniqueID STONEHEAVY = 3929807209U;
+                static const AkUniqueID STONELIGHT = 4103883880U;
+                static const AkUniqueID WOODHEAVY = 1697108119U;
+                static const AkUniqueID WOODLIGHT = 3912760598U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEP_TYPE
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
